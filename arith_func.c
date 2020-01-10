86d7fe4f2db63d1f0ce180f03809bff2139262d6
PRADEEPIKA13 <59741543+PRADEEPIKA13@users.noreply.github.com>
2020-01-10 22:48:49 +0530

    Update and rename sum_sub_func.c to arith_func.c

--- arith_func.c
+++ arith_func.c
@@ -2,17 +2,20 @@
 
 int sum(int,int);
 int sub(int,int);
+int mul(int,int);
 
 int main()
 {
-     int num1, num2, res,ans;
+     int num1, num2, res,ans,prod;
      scanf("%d %d", &num1, &num2);
      
      res = sum(num1, num2);
      ans = sub(num1,num2);
+     prod = mul(num1,num2);
  
-     printf("%d",res);
-     printf("%d",ans);
+     printf("%d\n",res);
+     printf("%d\n",ans);
+     printf("%d\n",prod);
      return 0;
 }
 
@@ -28,5 +31,12 @@ int sub(int a,int b)
      int c;
      c=a-b;
      return c;
+}
+
+int mul(int a,int b)
+{
+     int z;
+     z=x*y;
+     return z;
 }
 
