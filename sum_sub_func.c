f5cc23b9ceea6973deb198ebde96242c4a046f19
PRADEEPIKA13 <59741543+PRADEEPIKA13@users.noreply.github.com>
2020-01-10 22:39:04 +0530

    Update sum_sub_func.c

--- sum_sub_func.c
+++ sum_sub_func.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
 
 int sum(int,int);
+int sub(int,int);
 
 int main()
 {
-     int num1, num2, res;
+     int num1, num2, res,ans;
      scanf("%d %d", &num1, &num2);
      
      res = sum(num1, num2);
+     ans = sub(num1,num2);
  
      printf("%d",res);
+     printf("%d",ans);
      return 0;
 }
 
@@ -18,5 +21,12 @@ int sum(int x,int y)
     int z;
     z=x+y;
     return z;
+}
+
+int sub(int a,int b)
+{
+     int c;
+     c=a-b;
+     return c;
 }
 
