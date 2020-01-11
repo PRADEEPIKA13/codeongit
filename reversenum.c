1008ecbbcded16a4e4df14f86293f3d2afab1050
PRADEEPIKA13 <59741543+PRADEEPIKA13@users.noreply.github.com>
2020-01-11 23:03:42 +0530

    Update reversenum.c

--- reversenum.c
+++ reversenum.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main() 
 {
-     int n, rev = 0, remainder;
+     int n, rev = 0;
     printf("Enter an integer: ");
      scanf("%d", &n);
     while (n != 0) 
     {
-         remainder = n % 10;
-         rev = rev * 10 + remainder;
+         rev = n % 10;
+         rev = rev * 10 + r;
          n /= 10;
     }
     printf("Reversed number = %d", rev);
